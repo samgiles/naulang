e54a9ab185d63e724e513f49eecd34abe49bc3f0
Samuel Giles <aphro.cag@googlemail.com>
2014-01-09 03:34:10 +0000

    Altering test to a simple immutable assignment parsing test, this fails presently

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -1,15 +1,17 @@
 import pytest
+from rpython.rlib.parsing.parsing import ParseError
 from wlvlang.compiler.parser import parse_wlvlang
 
-def test_parse_if():
-    ifexpression = """
-    fn test() {
-        if x == y and x == b {
-            "hello";
-        } else {
-            "goodbye";
-        }
-    }"""
+def test_assignment_statement():
 
-    parse_wlvlang(ifexpression)
+    immutable_assignment = """
+    let x = 10;
+    let y = true;
+    """
+    try:
+        s = parse_wlvlang(immutable_assignment)
+    except ParseError, e:
+        pytest.fail(e.nice_error_message(source=immutable_assignment))
+
+    repr(s)
     assert True
