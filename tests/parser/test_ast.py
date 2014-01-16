7bac7c2fade974b5ed596da9180ae77e5802818c
Samuel Giles <aphro.cag@googlemail.com>
2014-01-16 17:26:52 +0000

    Added simple AST nodes and simple tests for WhileExpressions and IfExpressions.

--- tests/parser/test_ast.py
+++ tests/parser/test_ast.py
@@ -1,9 +1,24 @@
 import pytest
-from wlvlang.compiler.ast import Node, LetEq
+from wlvlang.compiler.ast import Node, LetEq, WhileExpression, IfExpression
 
 
 def test_leteq():
     subject = LetEq(Node(), "x");
 
-    assert(isinstance(subject.child_value_expression, Node))
-    assert(subject.child_symbol == "x")
+    assert(isinstance(subject.child_value_expression(), Node))
+    assert(subject.child_symbol() == "x")
+
+def test_whilexpression():
+    subject = WhileExpression(Node(), [Node(), Node()])
+
+    assert(isinstance(subject.child_condition_expr(), Node))
+    assert(isinstance(subject.child_bodystatements(), list))
+    assert(isinstance(subject.child_bodystatements()[0], Node))
+    assert(isinstance(subject.child_bodystatements()[1], Node))
+
+def test_ifexpression():
+    subject = IfExpression(Node(), [Node()], [Node()])
+
+    assert(isinstance(subject.child_condition_expr(), Node))
+    assert(isinstance(subject.child_truestatements(), list))
+    assert(isinstance(subject.child_falsestatements(), list))
--- wlvlang/compiler/ast.py
+++ wlvlang/compiler/ast.py
@@ -6,11 +6,39 @@ class LetEq(Node):
     """ Defines a node for the 'let [symbol] = [expression]' sub expression"""
 
     def __init__(self, value_expression, identifier):
-        self.child_value_expression = value_expression
-        self.child_symbol = identifier
+        self.value_expression = value_expression
+        self.symbol = identifier
 
     def child_value_expression(self):
-        return self.child_value_expression
+        return self.value_expression
 
     def child_symbol(self):
-        return self.child_symbol
+        return self.symbol
+
+class WhileExpression(Node):
+
+    def __init__(self, conditionexpression, bodystatements):
+        self.condition_expr = conditionexpression
+        self.bodystatements = bodystatements
+
+    def child_condition_expr(self):
+        return self.condition_expr
+
+    def child_bodystatements(self):
+        return self.bodystatements
+
+class IfExpression(Node):
+
+    def __init__(self, conditionexpression, truestatements, falsestatements):
+        self.condition_expr = conditionexpression
+        self.truestatement = truestatements
+        self.falsestatement = falsestatements
+
+    def child_condition_expr(self):
+        return self.condition_expr
+
+    def child_truestatements(self):
+        return self.truestatement
+
+    def child_falsestatements(self):
+        return self.falsestatement
