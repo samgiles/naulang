24481cc54c9698c92137577099fd9d1c8e3a243a
Samuel Giles <aphro.cag@googlemail.com>
2014-01-18 19:17:29 +0000

    Added some tests for the boolean primitive operations

--- tests/vmobjects/test_boolean.py
+++ tests/vmobjects/test_boolean.py
@@ -8,3 +8,67 @@ def test_get_value():
 
     assert(subjecta.get_value() == True)
     assert(subjectb.get_value() == False)
+
+def setup_primitive_test(left_val, right_val):
+    arec = ActivationRecord(2, None)
+
+    arec.push(Boolean(left_val))
+    arec.push(Boolean(right_val))
+
+    return arec
+
+def test_eq_primitive_true_true():
+    arec = setup_primitive_test(True, True)
+
+    _eq(None, arec, None)
+
+    value = arec.pop()
+    assert(value.get_value() == True)
+
+def test_eq_primitive_true_false():
+    arec = setup_primitive_test(True, False)
+
+    _eq(None, arec, None)
+
+    value = arec.pop()
+    assert(value.get_value() == False)
+
+def test_eq_primitive_false_true():
+    arec = setup_primitive_test(False, True)
+
+    _eq(None, arec, None)
+
+    value = arec.pop()
+    assert(value.get_value() == False)
+
+def test_or_primitive_true_true():
+    arec = setup_primitive_test(True, True)
+
+    _or(None, arec, None)
+
+    value = arec.pop()
+    assert(value.get_value() == True)
+
+def test_or_primitive_true_false():
+    arec = setup_primitive_test(True, False)
+
+    _or(None, arec, None)
+
+    value = arec.pop()
+    assert(value.get_value() == True)
+
+def test_or_primitive_false_true():
+    arec = setup_primitive_test(False, True)
+
+    _or(None, arec, None)
+
+    value = arec.pop()
+    assert(value.get_value() == True)
+
+def test_or_primitive_false_false():
+    arec = setup_primitive_test(False, False)
+
+    _or(None, arec, None)
+
+    value = arec.pop()
+    assert(value.get_value() == False)
