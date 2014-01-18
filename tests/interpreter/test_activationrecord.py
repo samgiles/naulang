918d513ae75d21c0a253c80fedf1fde68ee687ea
Samuel Giles <aphro.cag@googlemail.com>
2014-01-18 18:44:11 +0000

    Fixing tests, the activation record constructor arguments had changed

--- tests/interpreter/test_activationrecord.py
+++ tests/interpreter/test_activationrecord.py
@@ -2,12 +2,12 @@ import pytest
 from wlvlang.interpreter.activationrecord import ActivationRecord
 
 def test_get_previous_record():
-    subject = ActivationRecord(ActivationRecord(None))
+    subject = ActivationRecord(0, ActivationRecord(0, None))
 
     assert(isinstance(subject.get_previous_record(), ActivationRecord))
 
 def test_is_root_record():
-    subject = ActivationRecord(None)
+    subject = ActivationRecord(0, None)
 
     assert(True == subject.is_root_record())
 
