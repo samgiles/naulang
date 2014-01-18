6d6efbcc56e545565d287828f88b27d775faecc6
Samuel Giles <aphro.cag@googlemail.com>
2014-01-18 18:26:25 +0000

    Test _add primitive for integer

--- tests/vmobjects/test_integer.py
+++ tests/vmobjects/test_integer.py
@@ -2,7 +2,7 @@ import pytest
 
 from wlvlang.interpreter.activationrecord import ActivationRecord
 from wlvlang.vmobjects.integer import Integer
-from wlvlang.vmobjects.primitives.integer_primitive import _mul
+from wlvlang.vmobjects.primitives.integer_primitive import _mul, _add
 
 def test_get_value():
     subject = Integer(10)
@@ -21,3 +21,14 @@ def test_mul_primitive():
 
     value = arec.pop()
     assert(value.get_value() == 20000)
+
+def test_add_primitive():
+    arec = ActivationRecord(2, None)
+
+    arec.push(Integer(100))
+    arec.push(Integer(200))
+
+    _add(None, arec, None)
+
+    value = arec.pop()
+    assert(value.get_value() == 300)
