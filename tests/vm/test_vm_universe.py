d2452150ff1a811af920e44a69f530b23eb8a1ef
Samuel Giles <aphro.cag@googlemail.com>
2014-01-20 00:15:15 +0000

    Whitespace

--- tests/vm/test_vm_universe.py
+++ tests/vm/test_vm_universe.py
@@ -7,7 +7,6 @@ from wlvlang.vmobjects.classs import Class
 
 def test_new_integer():
     subject = VM_Universe()
-
     integer = subject.new_integer(10)
     assert(isinstance(integer, Integer))
     assert(integer.get_value() == 10)
