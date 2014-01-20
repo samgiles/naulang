ad2abcde8e07353d4e352fbabbbb69781e5637b0
Samuel Giles <aphro.cag@googlemail.com>
2014-01-20 00:13:17 +0000

    Test that the Integer primitive's class is created correctly and is returned when a call to the Integer's method get_class is made

--- tests/vm/test_vm_universe.py
+++ tests/vm/test_vm_universe.py
@@ -2,6 +2,7 @@ import pytest
 
 from wlvlang.vm.vm_universe import VM_Universe
 from wlvlang.vmobjects.integer import Integer
+from wlvlang.vmobjects.classs import Class
 
 
 def test_new_integer():
@@ -10,3 +11,9 @@ def test_new_integer():
     integer = subject.new_integer(10)
     assert(isinstance(integer, Integer))
     assert(integer.get_value() == 10)
+
+def test_new_integer_class():
+    subject = VM_Universe()
+    integer = subject.new_integer(10)
+    assert(isinstance(integer.get_class(subject), Class))
+    assert(integer.get_class(subject) == subject.integerClass)
