5f4924cb0ca68947ae0f40fb51d2b812a300b927
Samuel Giles <aphro.cag@googlemail.com>
2014-01-22 22:03:52 +0000

    Restarting parser and compiler work

---
+++ tests/compiler/test_parser.py
@@ -0,0 +1,36 @@
+
+
+def test_parse_constant_integer():
+    p = Parser()
+
+    assert p.parse("100") == ast.Main(ast.Block(ast.ConstantInteger(100)))
+    assert p.parse("-100") == ast.Main(ast.Block(ast.ConstantInteger(-100)))
+    assert p.parse("0x64") == ast.Main(ast.Block(ast.ConstantInteger(100)))
+    assert p.parse("0o144") == ast.Main(ast.Block(ast.ConstantInteger(100)))
+    assert p.parse("0b1100100") == ast.Main(ast.Block(ast.ConstantInteger(100)))
+
+def test_parse_constant_float():
+    p = Parser()
+
+    assert p.parse("100.0") == ast.Main(ast.Block(ast.ConstantFloat(100.0)))
+    assert p.parse("-100.0") == ast.Main(ast.Block(ast.ConstantFloat(-100.0)))
+    assert p.parse("1.0E2") == ast.Main(ast.Block(ast.ConstantFloat(100.0)))
+    assert p.parse("1.0e2") == ast.Main(ast.Block(ast.ConstantFloat(100.0)))
+
+def test_binary_expression():
+    p = Parser()
+
+    assert p.parse("100+100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "+", [ast.ConstantInteger(100)]))))
+    assert p.parse("100-100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "-", [ast.ConstantInteger(100)]))))
+    assert p.parse("100/100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "/", [ast.ConstantInteger(100)]))))
+    assert p.parse("100%100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "%", [ast.ConstantInteger(100)]))))
+    assert p.parse("100*100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "*", [ast.ConstantInteger(100)]))))
+    assert p.parse("100 == 100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "==", [ast.ConstantInteger(100)]))))
+    assert p.parse("100 != 100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "!=", [ast.ConstantInteger(100)]))))
+    assert p.parse("100 and 100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "and", [ast.ConstantInteger(100)]))))
+    assert p.parse("100 or 100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "or", [ast.ConstantInteger(100)]))))
+    assert p.parse("100 < 100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "<", [ast.ConstantInteger(100)]))))
+    assert p.parse("100 > 100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), ">", [ast.ConstantInteger(100)]))))
+    assert p.parse("100 >= 100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), ">=", [ast.ConstantInteger(100)]))))
+    assert p.parse("100 <= 100") == ast.Main(ast.Block(ast.Expression(ast.Send(ast.ConstantInteger(100), "<=", [ast.ConstantInteger(100)]))))
+
