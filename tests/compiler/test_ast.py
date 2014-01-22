045d36841df467ced2746fbca279a68a197bbbfc
Samuel Giles <aphro.cag@googlemail.com>
2014-01-22 22:15:27 +0000

    Added ConstantFloat AST node and tests

--- tests/compiler/test_ast.py
+++ tests/compiler/test_ast.py
@@ -3,3 +3,7 @@ from wlvlang.compiler import ast
 def test_ast_constant_integer():
     assert ast.ConstantInteger(10) == ast.ConstantInteger(10)
     assert ast.ConstantInteger(1231) != ast.ConstantInteger(123231)
+
+def test_ast_constant_float():
+    assert ast.ConstantFloat(1.0) == ast.ConstantFloat(1.0)
+    assert ast.ConstantFloat(1.1) != ast.ConstantFloat(1.0)
--- wlvlang/compiler/ast.py
+++ wlvlang/compiler/ast.py
@@ -9,3 +9,11 @@ class ConstantInteger(Node):
 
     def __eq__(self, other):
         return isinstance(other, ConstantInteger) and other._value == self._value
+
+class ConstantFloat(Node):
+
+    def __init__(self, value):
+        self._value = value
+
+    def __eq__(self, other):
+        return isinstance(other, ConstantFloat) and other._value == self._value
