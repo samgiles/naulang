f2ac1a3c752001291d370fbf0ae1369578270a0b
Samuel Giles <aphro.cag@googlemail.com>
2014-01-22 22:27:39 +0000

    Added send AST node, Send is the operation for invoking a method be it primitive or a user defined function

--- tests/compiler/test_ast.py
+++ tests/compiler/test_ast.py
@@ -7,3 +7,18 @@ def test_ast_constant_integer():
 def test_ast_constant_float():
     assert ast.ConstantFloat(1.0) == ast.ConstantFloat(1.0)
     assert ast.ConstantFloat(1.1) != ast.ConstantFloat(1.0)
+
+def test_ast_send():
+    assert ast.Send(ast.ConstantFloat(1.0), "+", [ast.ConstantFloat(1.0)]) == ast.Send(ast.ConstantFloat(1.0), "+", [ast.ConstantFloat(1.0)])
+    assert ast.Send(ast.ConstantInteger(1), "+", [ast.ConstantFloat(1.0)]) != ast.Send(ast.ConstantFloat(1.0), "+", [ast.ConstantFloat(1.0)])
+
+    # Test recursive equals
+    assert ast.Send(
+            ast.Send(ast.ConstantInteger(1), "*", [ast.ConstantInteger(1)]),
+            "*",
+            [ast.ConstantInteger(10)]) == ast.Send(ast.Send(ast.ConstantInteger(1), "*", [ast.ConstantInteger(1)]),"*", [ast.ConstantInteger(10)])
+
+    assert ast.Send(
+            ast.Send(ast.ConstantInteger(1), "*", [ast.ConstantInteger(1)]),
+            "/",
+            [ast.ConstantInteger(10)]) != ast.Send(ast.Send(ast.ConstantInteger(1), "*", [ast.ConstantInteger(1)]),"*", [ast.ConstantInteger(10)])
--- wlvlang/compiler/ast.py
+++ wlvlang/compiler/ast.py
@@ -17,3 +17,13 @@ class ConstantFloat(Node):
 
     def __eq__(self, other):
         return isinstance(other, ConstantFloat) and other._value == self._value
+
+class Send(Node):
+
+    def __init__(self, primaryexpression, message, arguments=[]):
+        self._primary = primaryexpression
+        self._message = message
+        self._arguments = arguments
+
+    def __eq__(self, other):
+        return isinstance(other, Send) and self._message == other._message and self._primary == other._primary and self._arguments == other._arguments
