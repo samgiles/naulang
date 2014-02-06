9ad73aa952edfb7b5944b63238d2f727a88c62ee
Samuel Giles <aphro.cag@googlemail.com>
2014-02-06 13:11:58 +0000

    Fix class name

--- tests/compiler/test_ast.py
+++ tests/compiler/test_ast.py
@@ -12,7 +12,7 @@ class DummyNode(ast.Node):
         return "DummyNode(%r)" % self.value
 
 def test_ast_integer_constant():
-    assert ast.IntegerConstant(10) == ast.ConstantInteger(10)
+    assert ast.IntegerConstant(10) == ast.IntegerConstant(10)
     assert ast.IntegerConstant(1231) != ast.IntegerConstant(123231)
 
 def test_ast_statement():
