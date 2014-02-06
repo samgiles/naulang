cbabe45fd492054b4707cf5d0578e7d1fff55147
Samuel Giles <aphro.cag@googlemail.com>
2014-02-06 13:07:52 +0000

    Begin testing the AST nodes

--- tests/compiler/test_ast.py
+++ tests/compiler/test_ast.py
@@ -1,7 +1,20 @@
 from wlvlang.compiler import ast
 
+class DummyNode(ast.Node):
+
+    def __init__(self, value):
+        self.value = value
+
+    def compile(self, context):
+        pass
+
+    def __repr__(self):
+        return "DummyNode(%r)" % self.value
+
 def test_ast_integer_constant():
     assert ast.IntegerConstant(10) == ast.ConstantInteger(10)
     assert ast.IntegerConstant(1231) != ast.IntegerConstant(123231)
 
-
+def test_ast_statement():
+    assert ast.Statement(DummyNode(True)) == ast.Statement(DummyNode(True))
+    assert ast.Statement(DummyNode(False)) != ast.Statement(DummyNode(True))
