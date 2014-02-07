4c88700e2edd634e6b08fc5c3829dbd9b04ce70c
Samuel Giles <aphro.cag@googlemail.com>
2014-02-07 09:45:22 +0000

    Test ast.IntegerConstant compilation

--- tests/compiler/test_ast.py
+++ tests/compiler/test_ast.py
@@ -5,6 +5,7 @@ from wlvlang.vm.vm_universe import VM_Universe
 from wlvlang.interpreter.bytecode import Bytecode
 
 from wlvlang.vmobjects.boolean import Boolean
+from wlvlang.vmobjects.integer import Integer
 
 class DummyNode(ast.Node):
 
@@ -26,6 +27,17 @@ def create_interpreter_context():
 def test_ast_integer_constant():
     assert ast.IntegerConstant(10) == ast.IntegerConstant(10)
     assert ast.IntegerConstant(1231) != ast.IntegerConstant(123231)
+
+def test_ast_integer_compile():
+    ctx = create_interpreter_context()
+    node = ast.IntegerConstant(100)
+    node.compile(ctx)
+
+    # Expect the constant to be stored in the literals area at position 0 (as this was a new context)
+    assert ctx._literals[0] == Integer(100)
+
+    # Expect the byte code to be [Bytecode.LOAD_CONST, 0]
+    assert ctx.bytecode == [Bytecode.LOAD_CONST, chr(0)]
 
 def test_ast_boolean_constant():
     assert ast.BooleanConstant(True) == ast.BooleanConstant(True)
