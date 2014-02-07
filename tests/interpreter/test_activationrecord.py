6a056e1c242f3195822461178666f5a87d373cc8
Samuel Giles <aphro.cag@googlemail.com>
2014-02-07 15:29:55 +0000

    Fixed the only test that currently exists for the arec.

--- tests/interpreter/test_activationrecord.py
+++ tests/interpreter/test_activationrecord.py
@@ -1,13 +1,9 @@
 import pytest
 from wlvlang.interpreter.activationrecord import ActivationRecord
 
-def test_get_previous_record():
-    subject = ActivationRecord(0, ActivationRecord(0, None))
-
-    assert(isinstance(subject.get_previous_record(), ActivationRecord))
 
 def test_is_root_record():
-    subject = ActivationRecord(0, None)
+    subject = ActivationRecord([], 0, 0, 0, None)
 
     assert(True == subject.is_root_record())
 
