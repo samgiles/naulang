8f6f214300cac0f4b0b42888e2938f37b2a17270
Samuel Giles <aphro.cag@googlemail.com>
2014-02-08 14:17:25 +0000

    Remove the ConstantFolding optimiser tests in preparation for a new interface using a separate tree transformer rather than hacking the current ASTVisitor implementation.

--- tests/compiler/test_optimisations.py
+++ tests/compiler/test_optimisations.py
@@ -2,15 +2,7 @@ from wlvlang.compiler import ast
 from wlvlang.compiler.optimiser import ConstantFolding
 
 def test_constant_integer_addop_folding():
-    cf_optimiser = ConstantFolding()
-    tree = ast.AddOp(ast.IntegerConstant(100), ast.IntegerConstant(200))
-
-    result = tree.accept(cf_optimiser)
-    assert result == ast.IntegerConstant(300)
+    pass
 
 def test_constant_integer_subtractop_folding():
-    cf_optimiser = ConstantFolding()
-    tree = ast.SubtractOp(ast.IntegerConstant(500), ast.IntegerConstant(200))
-
-    result = tree.accept(cf_optimiser)
-    assert result == ast.IntegerConstant(300)
+    pass
--- wlvlang/compiler/optimiser.py
+++ wlvlang/compiler/optimiser.py
@@ -3,17 +3,7 @@ from wlvlang.compiler import ast
 class ConstantFolding(ast.ASTVisitor):
 
     def visit_addop(self, node):
-
-        if isinstance(node._lhs, ast.IntegerConstant) and isinstance(node._rhs, ast.IntegerConstant):
-            folded_value = node._lhs._value + node._rhs._value
-            return ast.IntegerConstant(folded_value)
-
-        return node
+        pass
 
     def visit_subtractop(self, node):
-
-        if isinstance(node._lhs, ast.IntegerConstant) and isinstance(node._rhs, ast.IntegerConstant):
-            folded_value = node._lhs._value - node._rhs._value
-            return ast.IntegerConstant(folded_value)
-
-        return node
+        pass
