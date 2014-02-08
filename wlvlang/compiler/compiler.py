377adcab34d67145f34d95be920d576f19a3fcf3
Samuel Giles <aphro.cag@googlemail.com>
2014-02-08 16:23:10 +0000

    Attempting to plumb together the compiler and interpreter for rpython targeting

---
+++ wlvlang/compiler/compiler.py
@@ -0,0 +1,30 @@
+import os
+
+from rpython.rlib.streamio import open_file_as_stream
+from rpython.rlib.parsing.parsing import ParseError
+from rlib.string_stream import StringStream
+
+from wlvlang.compiler.sourceparser import parse
+from wlvlang.compiler.context import MethodCompilerContext
+
+def compile_source_from_file(path, filename, universe):
+    """ Given a source file, return a vmobjects.Method object """
+    fullname = path + os.sep + filename
+    try:
+        input_file = open_file_as_stream(fullname, "r")
+
+        try:
+            ast = parse(input_file.readall())
+        except ParseError, e:
+            os.write(2, e.nice_error_message(filename=fullname, source=source))
+            # Raise something less specific here (as we output)
+            raise e
+        finally:
+            input_file.close()
+    except OSError:
+        raise IOError()
+
+    compiler_context = MethodCompilerContext(universe)
+    ast.compile(compiler_context)
+    method = compiler_context.generate_method()
+    return method
