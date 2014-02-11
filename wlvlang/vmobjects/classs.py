c6ff32ac1f29fe1cefced58cc9d72d41eff333f9
SamGiles <aphro.cag@googlemail.com>
2014-02-11 19:47:45 +0000

    Work on the object space.. Added parent class method lookup

--- wlvlang/vmobjects/classs.py
+++ wlvlang/vmobjects/classs.py
@@ -1,9 +1,11 @@
 from wlvlang.vmobjects.object import Object
+from wlvlang.vm import exceptions
 class Class(Object):
 
-    def __init__(self, universe):
+    def __init__(self, universe, parent=None):
         self._invokabletable = {}
         self._universe = universe
+        self._parent = parent
 
     def lookup_invokable(self, signature):
         invokable = self._invokabletable.get(signature, None)
@@ -11,13 +13,20 @@ class Class(Object):
         if invokable:
             return invokable
 
-        # TODO: Super class chain
-        return None
+        if self._parent is None:
+            raise exceptions.MethodNotFoundException(signature, self)
+
+        return self._parent.lookup_invokable(signature)
 
     def add_primitives(self, primitives):
         """ Primitives should be a dictionary
         keyed by signature """
         self._invokabletable.update(primitives)
+
+    def add_method(self, method):
+        """ Adds a new Method to this class
+        definition """
+        self._invokabletable.set(method.signature, method)
 
     def get_name(self):
         return self._name
@@ -26,4 +35,4 @@ class Class(Object):
         self._name = name
 
     def get_class(self, universe):
-        universe.objectClass
+        return self
