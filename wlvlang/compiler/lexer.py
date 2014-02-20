24bd1afd6f98fb2283cbecc144a48fc7f933eccf
Samuel Giles <aphro.cag@googlemail.com>
2014-02-20 19:54:21 +0000

    Split lexer and parser

--- wlvlang/compiler/lexer.py
+++ wlvlang/compiler/lexer.py
@@ -2,52 +2,53 @@ from rply import LexerGenerator
 
 lg = LexerGenerator()
 
-
-lg.ignore('([\s\f\t\n\r\v]+)|(//[^\n]*\n)|(/\*([^\*]|\*[^/])*\*?\*/)')
+lg.ignore(r"([\s\f\t\n\r\v]+)|#.*$")
 
 def get_tokens():
     return [
-        ("LET", r"let"),
-        ("SINGLE_EQ", r"eq"),
+        # Keywords
         ("IF", r"if"),
-        ("ELSE", r"else"),
+        ("PRINT", r"print"),
+        ("FN", r"fn"),
         ("WHILE", r"while"),
         ("RETURN", r"return"),
-        ("TRUE", r"true"),
-        ("FALSE", r"false"),
-        ("INTEGER", r"-?0|[1-9][0-9]*"),
-        ("FLOAT",r"(((0|[1-9][0-9]*)(\.[0-9]*)?)|(\.[0-9]+))([eE][\+\-]?[0-9]*)?"),
-        ("SINGLE_STRING", r"'([^'\\]|\\.)*'"),
-        ("DOUBLE_STRING", r"\"([^\"\\]|\\.)*\""),
-        ("SELF", r"self"),
-        ("LPAREN", r"\("),
-        ("RPAREN", r"\)"),
-        ("IDENTIFIER", r"[a-zA-Z_$][a-zA-Z_0-9]*"),
-        ("FN", r"fn"),
-        ("LBRACE", r"\{"),
-        ("RBRACE", r"\}"),
-        ("COMMA", r","),
-        ("DOT", r"\."),
-        ("PLUS", r"\+"),
-        ("MINUS", r"-"),
+        ("LET", r"let"),
+        # Arithmetic Operators
         ("MUL", r"\*"),
         ("DIV", r"/"),
         ("MOD", r"%"),
-        ("CHAN_IN", r"<\+"),
-        ("CHAN_OUT", r"<-"),
-        ("IS", r"is"),
-        ("DOUBLE_EQ", "=="),
-        ("NOT_EQ", "!="),
-        ("LT", r"<"),
-        ("GT", r">"),
-        ("LTEQ", r"<="),
-        ("GTEQ", r">="),
-        ("BANG", r"!"),
-        ("PRINT", r"print"),
+        ("PLUS", r"\+"),
+        ("MINUS", r"-"),
+        ("NEGATE", r"-"),
+        # Logical Operators
         ("AND", r"and"),
         ("OR", r"or"),
         ("NOT", r"not"),
+        # Comparison Operators
         ("IS", r"is"),
+        ("DOUBLE_EQ", r"=="),
+        ("NOT_EQ", r"!="),
+        # Relational Operators
+        ("LTEQ", r"<="),
+        ("GTEQ", r">="),
+        ("LT", r"<"),
+        ("GT", r">"),
+        # Punctuation
+        ("LPAREN", r"\("),
+        ("RPAREN", r"\)"),
+        ("LBRACE", r"{"),
+        ("RBRACE", r"}"),
+        ("COMMA", r","),
+        ("LBRACK", r"\["),
+        ("RBRACK", r"\]"),
+        # Literals
+        ("TRUE", r"true"),
+        ("FALSE", r"false"),
+        ("FLOAT", r"(((0|[1-9][0-9]*)(\.[0-9]*)+)|(\.[0-9]+))([eE][\+\-]?[0-9]*)?"),
+        ("INTEGER", r"-?(0|[1-9][0-9]*)"),
+        ("IDENTIFIER", r"[a-zA-Z_$][a-zA-Z_0-9]*"),
+        # Others
+        ("EQUAL", r"="),
     ]
 
 tokens = get_tokens()
@@ -55,5 +56,7 @@ tokens = get_tokens()
 for token in tokens:
     lg.add(token[0], token[1])
 
-def create_lexer():
-    return lg.build()
+LEXER = lg.build()
+
+def get_lexer():
+    return LEXER
--- wlvlang/compiler/parser.py
+++ wlvlang/compiler/parser.py
@@ -2,64 +2,7 @@ from rply import ParserGenerator, LexerGenerator
 from rply.errors import LexingError
 
 from wlvlang.compiler import ast
-
-lg = LexerGenerator()
-
-lg.ignore(r"([\s\f\t\n\r\v]+)|#.*$")
-
-def get_tokens():
-    return [
-        # Keywords
-        ("IF", r"if"),
-        ("PRINT", r"print"),
-        ("FN", r"fn"),
-        ("WHILE", r"while"),
-        ("RETURN", r"return"),
-        ("LET", r"let"),
-        # Arithmetic Operators
-        ("MUL", r"\*"),
-        ("DIV", r"/"),
-        ("MOD", r"%"),
-        ("PLUS", r"\+"),
-        ("MINUS", r"-"),
-        ("NEGATE", r"-"),
-        # Logical Operators
-        ("AND", r"and"),
-        ("OR", r"or"),
-        ("NOT", r"not"),
-        # Comparison Operators
-        ("IS", r"is"),
-        ("DOUBLE_EQ", r"=="),
-        ("NOT_EQ", r"!="),
-        # Relational Operators
-        ("LTEQ", r"<="),
-        ("GTEQ", r">="),
-        ("LT", r"<"),
-        ("GT", r">"),
-        # Punctuation
-        ("LPAREN", r"\("),
-        ("RPAREN", r"\)"),
-        ("LBRACE", r"{"),
-        ("RBRACE", r"}"),
-        ("COMMA", r","),
-        ("LBRACK", r"\["),
-        ("RBRACK", r"\]"),
-        # Literals
-        ("TRUE", r"true"),
-        ("FALSE", r"false"),
-        ("FLOAT", r"(((0|[1-9][0-9]*)(\.[0-9]*)+)|(\.[0-9]+))([eE][\+\-]?[0-9]*)?"),
-        ("INTEGER", r"-?(0|[1-9][0-9]*)"),
-        ("IDENTIFIER", r"[a-zA-Z_$][a-zA-Z_0-9]*"),
-        # Others
-        ("EQUAL", r"="),
-    ]
-
-tokens = get_tokens()
-
-for token in tokens:
-    lg.add(token[0], token[1])
-
-lexer = lg.build()
+from wlvlang.compiler.lexer import get_tokens, get_lexer
 
 tokentypes, _ = zip(*get_tokens())
 
@@ -292,4 +235,4 @@ def create_parser():
     return pg.build()
 
 def create_lexer():
-    return lg.build()
+    return get_lexer()
