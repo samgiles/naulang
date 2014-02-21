5f57bc926dbc6b6e719e7ac0687dc61861d32a0b
Samuel Giles <aphro.cag@googlemail.com>
2014-02-21 15:03:03 +0000

    Added test runner that optionally outputs JUnit style XML

--- tests/functional/wlvtest.py
+++ tests/functional/wlvtest.py
@@ -5,6 +5,8 @@ import subprocess
 import os
 import glob
 
+from junit_xml import TestSuite, TestCase
+
 from os import kill
 from signal import alarm, signal, SIGALRM, SIGKILL
 from subprocess import PIPE, Popen
@@ -14,6 +16,7 @@ def parse_arguments():
     parser.add_argument('wlvlang_interpreter', nargs=1,
                        help='the interpreter executable to use to run the programs against')
     parser.add_argument('test_directory', nargs=1, help='the directory in which the .wlt files exist')
+    parser.add_argument("--xml", help="output JUnit style XML", action="store_true")
     args = parser.parse_args()
     return args
 
@@ -56,12 +59,37 @@ if __name__ == '__main__':
 
     executable = args.wlvlang_interpreter[0]
     test_directory = args.test_directory[0]
+    xml_out = args.xml
+    test_cases = []
+
+
+    # TODO: Timing on the test cases in XML
+    def log_errord(test_name, stderr, stdout):
+        if xml_out:
+            tc = TestCase(test_name, executable, 0, stdout, stderr)
+            tc.add_error_info(message="Test Error\n", output=stdout)
+            test_cases.append(tc)
+        else:
+            print "Test %s failed:\nstderr:\n %s\nstdout:\n%s\n----------\n" % (test_name, stderr, stdout)
+
+    def log_failed(test_name, actual, expected):
+        if xml_out:
+            tc = TestCase(test_name, executable, 0, actual, "")
+            tc.add_failure_info(message="Test Failed\nExpected:\n%s\nActual:\n%s" % (expected, actual))
+            test_cases.append(tc)
+        else:
+            print "Test %s failed:\nexpected:\n%s\nactual:\n%s\n---------\n" % (test_name, expected, actual)
+
+    def log_passed(test_name):
+        if xml_out:
+            test_cases.append(TestCase(test_name, executable, 0, "", ""))
+        else:
+            print "Test %s passed\n" % test_name
 
     test_files = glob.glob1(test_directory, "*.wl")
 
     failed = False
     for test_file in test_files:
-        print "Testing %r" % test_file
 
         # Get the test_name from the file name so we can determine which spec file we should test it against
         test_name = os.path.splitext(test_file)[0]
@@ -75,16 +103,22 @@ if __name__ == '__main__':
 
         if code != 0:
             failed = True
-            print "Test %s failed:\nstderr:\n %s\nstdout:\n%s\n----------\n" % (test_name, serr, sout)
+            log_errord(test_name, serr, sout)
         else:
             with open(expected_output, "r") as f:
                     expected=f.read()
 
             if sout != expected:
                 failed = True
-                print "Test %s failed:\nexpected:\n%s\nactual:\n%s\n---------\n" % (test_name, expected, sout)
+                log_failed(test_name, sout, expected)
+            else:
+                log_passed(test_name)
 
-    if failed:
+    if xml_out:
+        test_suite = TestSuite("Functional Tests", test_cases)
+        print TestSuite.to_xml_string([test_suite])
+
+    if failed and not xml_out:
         sys.exit(1)
     else:
         sys.exit(0)
