c9cf24cb58396fe0821436c0323e64e7e708c134
Samuel Giles <aphro.cag@googlemail.com>
2014-02-21 12:24:43 +0000

    Same as previous, removing test_optimisations until I've written the optimisers

--- tests/compiler/test_optimisations.py
+++
@@ -1,8 +0,0 @@
-from wlvlang.compiler import ast
-from wlvlang.compiler.optimiser import ConstantFolding
-
-def test_constant_integer_addop_folding():
-    pass
-
-def test_constant_integer_subtractop_folding():
-    pass
