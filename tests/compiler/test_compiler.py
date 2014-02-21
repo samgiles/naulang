07295f7bb5d3d83ef4f21adbb653d6f27f434b94
Samuel Giles <aphro.cag@googlemail.com>
2014-02-21 16:37:42 +0000

    Function arg list should be a list

--- tests/compiler/test_compiler.py
+++ tests/compiler/test_compiler.py
@@ -281,7 +281,7 @@ def test_invoke_global_list():
     ctx = create_interpreter_context()
     t = create_syntax_directed_translator(ctx)
 
-    node = ast.FunctionCall(ast.IdentifierExpression('list'), ast.FunctionArgList(ast.IntegerConstant(10)))
+    node = ast.FunctionCall(ast.IdentifierExpression('list'), ast.FunctionArgList([ast.IntegerConstant(10)]))
     node.accept(t)
 
     assert ctx.bytecode == [Bytecode.LOAD_CONST, chr(0), Bytecode.INVOKE_GLOBAL, chr(0)]
