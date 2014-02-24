a184aabc2e6aa9e669b8b0bcaac148fceea68cbe
Samuel Giles <aphro.cag@googlemail.com>
2014-02-24 23:23:12 +0000

    Integer object

---
+++ wlvlang-distilled/interpreter/objectspace/integer.py
@@ -0,0 +1,25 @@
+from wlvlang.interpreter.objectspace.object import Object
+
+class Integer(Object):
+
+    def __init__(self, value):
+        self.value = value
+
+    def get_integer_value(self):
+        return self._value
+
+    def get_as_string(self):
+        return str(self)
+
+    def get_class(self, universe):
+        # Not keen on this coupling
+        return universe.integerClass
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return "int(%s)" % (str(self))
+
+    def __eq__(self, other):
+        return isinstance(other, Integer) and self.value == other.value
