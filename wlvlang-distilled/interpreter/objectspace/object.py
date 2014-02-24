89bd3985a40f3e3acb5f7873c93f8a66582b2246
Samuel Giles <aphro.cag@googlemail.com>
2014-02-24 23:18:58 +0000

    Code clean up base object

---
+++ wlvlang-distilled/interpreter/objectspace/object.py
@@ -0,0 +1,29 @@
+class Object(object):
+    """ The base object for all types of object that
+    are handled or manipulated within the interpreter.
+
+    All operations are invoked by sending a message to
+    an object. The concrete implementation of the object
+    will dispatch to a specific method implemented by the
+    object.
+    """
+
+    def __init__(self):
+        self.slots = {}
+
+    def send(self, activation_record, selector, arguments, vm_universe, interpreter):
+        activation_record.push(self)
+
+        invokable = self.get_class(object_space).lookup_invokable(selector)
+
+        if not invokable:
+            raise OperationNotFound()
+
+        for arg in arguments:
+            activation_record.push(arg)
+
+        invokable(self, activation_record, interpreter)
+
+
+    def get_class(self, universe):
+        pass
