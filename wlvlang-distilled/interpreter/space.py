4946cf36cd53251a11481a6ddccec1bb2730663f
Samuel Giles <aphro.cag@googlemail.com>
2014-02-24 23:14:53 +0000

    Code clean up, removing JIT references/hints

--- wlvlang-distilled/interpreter/space.py
+++ wlvlang-distilled/interpreter/space.py
@@ -5,25 +5,13 @@ from wlvlang.vmobjects.boolean import Boolean
 from wlvlang.vmobjects.string import String
 from wlvlang.vmobjects.array import Array
 
-from wlvlang.vm.symbol_table import SymbolTable
 
-from rpython.rlib import jit
-
-
-class VM_Universe(object):
+class ObjectSpace(object):
 
     def __init__(self):
         self.primitive_functions = []
-        self._symbol_table = SymbolTable()
-        self.integerClass = None
-        self.methodClass = None
-        self.booleanClass = None
-        self.arrayClass = None
-        self.primitiveClass = None
-        self.stringClass = None
-        self.initialise_primitives()
 
-    def initialise_primitives(self):
+        # Classes define the operations that can be performed on a type
         self.integerClass = Class(self)
         self.methodClass = Class(self)
         self.booleanClass = Class(self)
@@ -31,6 +19,9 @@ class VM_Universe(object):
         self.primitiveClass = Class(self)
         self.stringClass = Class(self)
 
+        self.initialise_primitives()
+
+    def initialise_primitives(self):
         # Initialise prims
         initialise_primitives(self)
 
@@ -40,19 +31,14 @@ class VM_Universe(object):
     def add_primitive_function(self, index, function):
         self.primitive_functions[index] = function
 
-
-    @jit.elidable
     def new_integer(self, value):
         return Integer(value)
 
-    @jit.elidable
     def new_boolean(self, value):
         return Boolean(value)
 
-    @jit.elidable
     def new_array(self, initial_size):
         return Array(initial_size)
 
-    @jit.elidable
     def new_string(self, value):
         return String(value)
