6a4bf5beeb4321013b588e66a8e096768ba751e5
Samuel Giles <aphro.cag@googlemail.com>
2014-02-27 22:02:30 +0000

    Fixed STORE test

--- tests/interpreter/test_interpreter.py
+++ tests/interpreter/test_interpreter.py
@@ -58,10 +58,10 @@ def test_bc_STORE():
             Store the local on top of the stack in it's respective
             position
     """
-    uv, interpreter = create_universe_and_interpreter()
+    space, interpreter = create_space_and_interpreter()
     method = create_test_method([], [None], [Bytecode.STORE, 0, Bytecode.HALT])
     arec = create_arec(method, 1)
-    arec.push(uv.new_integer(100))
+    arec.push(space.new_integer(100))
     interpreter.interpret(method, arec)
 
     assert arec.get_local_at(0) == Integer(100)
