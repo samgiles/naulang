1df49d140e0df2db4093e36f9c108b40ef24d9ec
Samuel Giles <aphro.cag@googlemail.com>
2014-02-27 22:19:50 +0000

    Fixed arec test

--- tests/interpreter/test_activationrecord.py
+++ tests/interpreter/test_activationrecord.py
@@ -3,7 +3,7 @@ from wlvlang.interpreter.activationrecord import ActivationRecord
 
 
 def test_is_root_record():
-    subject = ActivationRecord([], 0, 0, 0, None)
+    subject = ActivationRecord([], [], 0, None)
 
     assert(True == subject.is_root_record())
 
