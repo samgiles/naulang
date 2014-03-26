e440c9ca19185ea5ea84cbcfe39e0cb0477d67a8
SamGiles <aphro.cag@googlemail.com>
2014-03-26 14:16:12 +0000

    Refactored the compiler method introducing some indirection when setting up the command line arguments. This is a hacky place to be doing this but we need to register the args symbol in the compiler context.

--- wlvlang/compiler/compiler.py
+++ wlvlang/compiler/compiler.py
@@ -15,7 +15,6 @@ from rpython.rlib.streamio import open_file_as_stream
 lexer = get_lexer()
 parser = create_parser()
 
-
 def _print_syntax_error_message(message, source_position, source):
     lineno = source_position.lineno
     colno = source_position.colno
@@ -49,24 +48,30 @@ def compile_file_with_arguments(filename, object_space, command_line_arguments=[
         os.write(2, "%s: %s\n" % (os.strerror(msg.errno), fullname))
         raise IOError()
 
-    # Set up compilation context
     compiler_context = FunctionCompilerContext(object_space)
 
-    # Add file arguments into 'args' array parameter
-    array = object_space.new_array(len(command_line_arguments))
+    arguments_array = _create_commandline_arguments_array(object_space, command_line_arguments)
+    arguments_local_offset = _register_symbol_in_compiler_context(compiler_context, symbol="args")
 
-    i = len(command_line_arguments) - 1
-    while i >= 0:
-        array.set_value_at(i, object_space.new_string(str(command_line_arguments[i])))
-        i -= 1
-
-    arg_local = compiler_context.register_local("args")
-
-    # Translate
-    sdt = SyntaxDirectedTranslator(compiler_context)
-    ast.accept(sdt)
+    translator = SyntaxDirectedTranslator(compiler_context)
+    ast.accept(translator)
 
     # Ensure the bytecode is halting
     compiler_context.emit(Bytecode.HALT)
 
-    return compiler_context.generate_method(), arg_local, array
+    return compiler_context.generate_method(), arguments_local_offset, arguments_array
+
+def _create_commandline_arguments_array(object_space, command_line_arguments=[]):
+    argument_array = object_space.new_array(len(command_line_arguments))
+
+    argument_index = len(command_line_arguments) - 1
+
+    while argument_index >= 0:
+        argument_array.set_value_at(argument_index, object_space.new_string(str(command_line_arguments[argument_index])))
+        argument_index -= 1
+
+    return argument_array
+
+def _register_symbol_in_compiler_context(compiler_context, symbol):
+    return compiler_context.register_local(symbol)
+
