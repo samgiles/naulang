c0f0b1eef0c13decfc314a1d4ebd74119240a6f9
Samuel Giles <aphro.cag@googlemail.com>
2014-03-27 03:50:42 +0000

    Fixed test_ssa

--- tests/compiler/test_ssa.py
+++ tests/compiler/test_ssa.py
@@ -1,4 +1,4 @@
-from wlvlang.compiler.ssa import TACGen, Operator, GraphNode
+from wlvlang.compiler.ssa import TACGen
 from wlvlang.compiler import ast
 from wlvlang.interpreter.space import ObjectSpace
 
@@ -26,17 +26,16 @@ def test_addition_expression():
 
     node.accept(tac)
     tacs = tac.get_tacs()
-    import pytest;pytest.set_trace()
     assert tac.get_tacs() == {
-        "v0": (Operator.CONST, 10, None),
-        "v1": (Operator.CONST, 6, None),
-        "v2": (Operator.MUL, "v0", "v1"),
-        "v3": (Operator.CONST, 5, None),
-        "v4": (Operator.SUB, "v2", "v3"),
-        "v5": (Operator.CONST, 2, None),
-        "v6": (Operator.ADD, "v4", "v5"),
-        "v7": (Operator.CONST, 1000, None),
-        "v8": (Operator.CONST, 2, None),
-        "v9": (Operator.DIV, "v7", "v8"),
-        "v10": (Operator.ADD, "v6", "v9")
+        "v0": (ast.Operator.CONST, 10, None),
+        "v1": (ast.Operator.CONST, 6, None),
+        "v2": (ast.Operator.MUL, "v0", "v1"),
+        "v3": (ast.Operator.CONST, 5, None),
+        "v4": (ast.Operator.SUB, "v2", "v3"),
+        "v5": (ast.Operator.CONST, 2, None),
+        "v6": (ast.Operator.ADD, "v4", "v5"),
+        "v7": (ast.Operator.CONST, 1000, None),
+        "v8": (ast.Operator.CONST, 2, None),
+        "v9": (ast.Operator.DIV, "v7", "v8"),
+        "v10": (ast.Operator.ADD, "v6", "v9")
     }
