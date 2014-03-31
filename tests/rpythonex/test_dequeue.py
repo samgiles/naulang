cdce1fd49361cff90ee8b835fe3f1021964b4e11
Samuel Giles <aphro.cag@googlemail.com>
2014-03-31 21:51:52 +0100

    Added tests for steal and push

--- tests/rpythonex/test_dequeue.py
+++ tests/rpythonex/test_dequeue.py
@@ -13,10 +13,36 @@ def test_push_bottom():
 def test_push_bottom_filled():
     deq_logsize = 4
     deq = Deq(deq_logsize)
-    import pdb;pdb.set_trace()
 
     for i in range((0x1 << deq_logsize) + 1):
         deq.push_bottom(i)
 
     for i in range((0x1 << deq_logsize), -1, -1):
+        assert deq.pop_bottom() == i
+
+def test_steal():
+    deq_logsize = 4
+    deq = Deq(deq_logsize)
+
+    for i in range((0x1 << deq_logsize) - 1):
+        deq.push_bottom(i)
+
+    for i in range((0x1 << deq_logsize) - 1):
+        assert deq.steal() == i
+
+def test_steal_and_push_doesnt_grow():
+    deq_logsize = 4
+    deq = Deq(deq_logsize)
+
+    for i in range((0x1 << deq_logsize) - 1):
+        deq.push_bottom(i)
+
+    # Assert that the queue has not grown in size (even though it is full)
+    assert deq.size() == 0x1 << deq_logsize
+    assert deq.steal() == 0
+
+    deq.push_bottom((0x1 << deq_logsize) - 1)
+    assert deq.size() == 0x1 << deq_logsize
+
+    for i in range((0x1 << deq_logsize) - 1, 0, -1):
         assert deq.pop_bottom() == i
