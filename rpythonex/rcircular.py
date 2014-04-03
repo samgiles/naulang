7c397f5df875f2acde8a7cef6a5e622e68a06223
Samuel Giles <aphro.cag@googlemail.com>
2014-04-03 23:21:08 +0100

    Size is elidable

--- rpythonex/rcircular.py
+++ rpythonex/rcircular.py
@@ -2,11 +2,14 @@ from rpython.rlib import jit
 class CircularArray(object):
     """ Simple Circular Array implementation based on implementation by Chase and Lev (2005)  http://dx.doi.org/10.1145/1073970.1073974
     """
+
     _immutable_ = True
+    _immutable_fields_ = ["_segment", "_logarithmic_size"]
     def __init__(self, logarithmic_size, initial_value=None):
         self._logarithmic_size = logarithmic_size
         self._segment = [initial_value] * (0x01 << self._logarithmic_size)
 
+    @jit.elidable
     def size(self):
         return 1 << self._logarithmic_size
 
