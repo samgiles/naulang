c6dcbbcc70039e9c8c7c99e3623d97e3223d73c3
Samuel Giles <aphro.cag@googlemail.com>
2014-04-06 23:43:32 +0100

    Async/buffered channel behaviour tests, the running speed of these is very different for the tokenring benchmark

---
+++ tests/benchmarks/baselines/tokenring_async.go
@@ -0,0 +1,103 @@
+/*
+ * Go benchmark
+ * ------------
+ *
+ * Author: C.G. Ritson
+ * License: GPL v2
+ * Date: 14/11/2009
+ * Origin: http://github.com/concurrency/kroc
+ *
+ */
+
+package main
+
+import (
+	"fmt";
+	"flag";
+	"os";
+	"runtime";
+	"strconv";
+	"time";
+)
+
+const ELEMENTS = 256
+
+func element(this, next chan int) {
+	for {
+		token := <-this;
+		if token > 0 {
+			next <- token + 1
+		} else {
+			next <- token;
+			return
+		}
+	}
+}
+
+func root(cycles, tokens int, this, next chan int) {
+	next <- 1;
+	token := <-this;
+
+
+	for i := 0; i < tokens; i = i + 1 {
+		next <- i + 1
+	}
+	for cycles > 0 {
+		for i := 0; i < tokens; i = i + 1 {
+			token = <-this;
+			next <- token + 1
+		}
+		cycles = cycles - 1
+	}
+	sum := 0;
+	for i := 0; i < tokens; i = i + 1 {
+		sum = sum + <-this
+	}
+
+
+	next <- 0;
+	<-this;
+}
+
+func ring(cycles, tokens int) {
+	head := make(chan int, 10);
+	this := head;
+
+	for i := 0; i < ELEMENTS - 1; i = i + 1 {
+		next := make(chan int, 10);
+		go element(this, next);
+		this = next
+	}
+
+	root(cycles, tokens, this, head)
+}
+
+func main() {
+	flag.Parse();
+
+	threads, _ := strconv.Atoi(os.Getenv("CORES"));
+	if threads < 1 {
+		threads = 1
+	}
+	runtime.GOMAXPROCS(threads);
+
+	cycles := 0;
+	if flag.NArg() >= 1 {
+		cycles, _ = strconv.Atoi(flag.Arg(0))
+	}
+	tokens := 1;
+	if flag.NArg() >= 2 {
+		tokens, _ = strconv.Atoi(flag.Arg(1))
+	}
+	sum := int64(0)
+	tries := 10
+
+	for i := 0; i < tries; i++ {
+		t0 := time.Now()
+		ring(cycles, tokens)
+		t1 := time.Now()
+		sum = sum + int64(t1.Sub(t0))
+	}
+
+	fmt.Printf("go-tokenring(%d, %d): total: iterations=%d runtime:%dus\n",cycles, tokens, tries, sum / int64(time.Microsecond))
+}
---
+++ tests/benchmarks/tokenring_async.wl
@@ -0,0 +1,86 @@
+let ELEMENTS = 256
+let cycles = int(args[1])
+let tokens = int(args[2])
+
+
+let element = fn(this, next) {
+    while true {
+        let token = <:this
+        if token > 0 {
+            next <- token + 1
+            continue
+        }
+
+        next <- token
+        break
+    }
+}
+
+let root = fn(cycles, tokens, this, next) {
+    next <- 1
+    let token = <:this
+
+    let i = 0
+    while i < tokens {
+        i = i + 1
+        next <- i
+    }
+
+    while cycles > 0 {
+        let i = 0
+        while i < tokens {
+            i = i + 1
+            token = <:this
+            next <- token + 1
+        }
+        cycles = cycles - 1
+    }
+
+    let sum = 0
+    i = 0
+    while i < tokens {
+        i = i + 1
+        sum = sum + <:this
+    }
+
+    next <- 0
+    token = <:this
+}
+
+let ring = fn(cycles, tokens) {
+    let head = async_chan()
+    let this = head
+    let next = 0
+    let i = 0
+    let elems = ELEMENTS
+    while i < elems {
+        i = i + 1
+        next = async_chan()
+        async element(this, next)
+        this = next
+    }
+
+    root(cycles, tokens, this, head)
+}
+
+
+let warmup = 5
+let i = warmup
+
+while i >= 0 {
+    i = i - 1
+    ring(cycles, tokens)
+}
+
+let sum = 0
+let tries = 10
+i = tries
+while i > 0 {
+    i = i - 1
+    let start = time()
+    ring(cycles, tokens)
+    let end = time()
+    sum = sum + (end - start)
+}
+
+print "nau-tokenring(" + cycles + ", " + tokens + "): total: iterations=" + tries + " runtime: " + sum +"us"
