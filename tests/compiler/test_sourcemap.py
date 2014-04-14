8e8ad536db22aa3ca104f520b4cfad110528e161
SamGiles <aphro.cag@googlemail.com>
2014-04-14 23:36:30 +0100

    Added simple test for sourcemap interface so fat

---
+++ tests/compiler/test_sourcemap.py
@@ -0,0 +1,10 @@
+from wlvlang.compiler.sourcemap import SourceMap
+
+class DummyCodePosition:
+    def __init__(self, val):
+        self.val = val
+
+def test_get_and_add():
+    sourcemap = SourceMap()
+    sourcemap.add(12, DummyCodePosition(909))
+    assert sourcemap.get(12).val == 909
